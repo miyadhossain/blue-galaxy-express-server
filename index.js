26f1983d19b04582c27a00bd7e1a1fa8deca7406
miyadhossain <miyad.mh@gmail.com>
2021-04-15 18:13:27 +0600

    user admin

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ client.connect((err) => {
   const servicesCollection = client
     .db("logisticService")
     .collection("services");
+  const bookingsCollection = client
+    .db("logisticService")
+    .collection("bookings");
+  const reviewsCollection = client.db("logisticService").collection("reviews");
 
   // post database
   app.post("/addService", (req, res) => {
@@ -33,6 +37,47 @@ client.connect((err) => {
     servicesCollection.find().toArray((err, items) => {
       res.send(items);
     });
+  });
+
+  // to read product by keys
+  app.get("/book/:id", (req, res) => {
+    servicesCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  // order placed
+  app.post("/addBookings", (req, res) => {
+    const newBooking = req.body;
+    bookingsCollection.insertOne(newBooking).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // user review
+  app.post("/addReviews", (req, res) => {
+    const newReview = req.body;
+    reviewsCollection.insertOne(newReview).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // read or get reviews
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  // get data by email query
+  app.get("/bookings", (req, res) => {
+    bookingsCollection
+      .find({ email: req.query.email })
+      .toArray((err, items) => {
+        res.send(items);
+      });
   });
 
   console.log("MongoDB connect successfully");
